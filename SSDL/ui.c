ed5b01e088d7b0074432daafe9d2c7e77742c8a4
diptivm <diptivm08@gmail.com>
2016-11-18 20:10:32 -0800

    Added blank ui.c and ui.h files

---
+++ SSDL/ui.c
@@ -0,0 +1,5 @@
+#include "main.h"                 // Application header
+#include "ui.h"                 // Good to self-reference
+#include "salvo.h"                // Req'd because we call e.g. OSDelay() 
+
+
