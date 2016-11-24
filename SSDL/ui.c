d8a08b189fbe9c6fd885ed3a7083fdad885ab54c
diptivm <diptivm08@gmail.com>
2016-11-23 20:46:55 -0800

    Decipher command using switch case

--- SSDL/ui.c
+++ SSDL/ui.c
@@ -13,14 +13,56 @@ TaskUI()
 ****                                                                       ****
 ******************************************************************************/
 void TaskUI(void) {
-  unsigned char cmd;
+  unsigned char inpChar;
   MsgTS(STR_TASK_UI ": Starting.");
 
   while(1) {
     //OS_Delay(200);
     OS_WaitSem(SEM_UI_CHAR, OSNO_TIMEOUT);
-    cmd = usart_uart1_getchar();
-    sprintf(strTmp, STR_TASK_UI ": Hello from TaskUI. Read %d", cmd);
+    inpChar = usart_uart1_getchar();
+    sprintf(strTmp, STR_TASK_UI ": Hello from TaskUI. Read %d", inpChar);
     MsgTS(strTmp);
+
+    switch(tolower(inpChar)) {
+      case 'h':
+      case '?':
+        sprintf(strTmp, STR_TASK_UI ": Read h");
+        MsgTS(strTmp);
+        break;
+
+      case 'i':
+        sprintf(strTmp, STR_TASK_UI ": Read i");
+        MsgTS(strTmp);
+        break;
+  
+      case 'r':
+        sprintf(strTmp, STR_TASK_UI ": Read r");
+        MsgTS(strTmp);
+        break;
+
+      case 't':
+        sprintf(strTmp, STR_TASK_UI ": Read t");
+        MsgTS(strTmp);
+        break;
+
+      case 'v':
+        sprintf(strTmp, STR_TASK_UI ": Read v");
+        MsgTS(strTmp);
+        break;
+
+      case '1':
+      case '2':
+      case '3':
+      case '4':
+      case '5':
+      case '6':
+      case '7':
+        sprintf(strTmp, STR_TASK_UI ": Read number");
+        MsgTS(strTmp);
+        break;
+
+      default:
+        break;
+    }
   }
 }
